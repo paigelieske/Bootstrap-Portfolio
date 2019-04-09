8859b7729a4cab22cae8472ea7996633d1deb8b8
Paige Lieske <paigeproctor@comcast.net>
2019-04-08 23:17:39 -0400

    updated portfolio

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="paigeproctor@comcast.net";
+    $subject="Form to email message";
+    $name=$_POST["name"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $name\nEmail: $email\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
